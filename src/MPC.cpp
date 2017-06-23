76e180ed75125a6f1832ac5f448555dc36add479
Doug Guastaferro <dguastaferro@gmail.com>
2017-06-23 08:19:33 -0700

    Tuned params

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,8 +6,8 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 8;
-double dt = 0.1;
+size_t N = 9;
+double dt = 0.17;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -51,15 +51,15 @@ class FG_eval {
 
     // The part of the cost based on the reference state.
     for (int t = 0; t < N; t++) {
-      fg[0] += 3000*CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += 3000*CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 4000*CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += 4000*CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
     // Minimize the use of actuators.
     for (int t = 0; t < N - 1; t++) {
-      fg[0] += 5*CppAD::pow(vars[delta_start + t], 2);
-      fg[0] += 5*CppAD::pow(vars[a_start + t], 2);
+      fg[0] += 3*CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 3*CppAD::pow(vars[a_start + t], 2);
     }
 
     // Minimize the value gap between sequential actuations.
